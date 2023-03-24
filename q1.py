d7fdaeebf4c4d196e15d565b1ac2f4354abdb743
James Chung <sunhochung53@gmail.com>
2023-03-24 03:04:59 -0700

    Use objective function

--- q1.py
+++ q1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from scipy.stats import poisson
+from scipy.stats import poisson, gamma
 from scipy.special import factorial
 
 df = pd.read_csv('./pmm_q1.tsv')
@@ -34,6 +34,9 @@ def calculate_pi(cluster):
 ## Initial parameters setup
 initial_lambdas = [poissonMLE(cluster_0), poissonMLE(cluster_1), poissonMLE(cluster_2)]
 initial_pis = [calculate_pi(cluster_0), calculate_pi(cluster_1), calculate_pi(cluster_2)]
+
+## Objective function values
+## log likelihoods + summation of log priors
 log_likelihoods = []
 
 lambda_history = []
@@ -43,15 +46,19 @@ pi_history.append(initial_pis)
 
 def calculate_log_ll(lambdas, pis):
   log_ll = 0
+  log_prior = 0
   for i in range(len(data)):
     x_i = data[i][0]
-    sub_sum = 0
+    ll_sum = 0
     for k in range(3):
       lambda_k = lambdas[k]
       pi_k = pis[k]
-      sub_sum += pi_k * poisson.pmf(x_i, lambda_k)
-    log_ll += math.log(sub_sum)
-  log_likelihoods.append(log_ll)
+      ll_sum += pi_k * poisson.pmf(x_i, lambda_k)
+    log_ll += math.log(ll_sum)
+  for k in range(3):
+    lambda_k = lambdas[k]
+    log_prior += math.log(gamma.pdf(lambda_k, 2, loc=0, scale=0.5))
+  log_likelihoods.append(log_ll + log_prior)
 
 calculate_log_ll(initial_lambdas, initial_pis)
 
@@ -108,25 +115,34 @@ while delta > epsilon:
   executeEM()
   delta = abs(log_likelihoods[-1] - log_likelihoods[-2])
 
-print("Initial lambdas: " + str(initial_lambdas))
+print("\nInitial lambdas: " + str(initial_lambdas))
 print("Final lambdas: " + str(lambda_history[-1]))
-print("\n")
-print("Initial pis: " + str(initial_pis))
+print("\nInitial pis: " + str(initial_pis))
 print("Final pis: " + str(pi_history[-1]))
 
-fig, ax1 = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(1, 2)
 x = np.arange(0, 26, 0.1)
+
 for k in range(3):
   lambda_k = lambda_history[-1][k]
   pmf = np.exp(-lambda_k) * np.power(lambda_k, x) / factorial(x)
   pmf = np.round(pmf, 5)
   ax1.plot(x, pmf)
 
-ax2 = ax1.twinx()
-n, bins, patches = ax2.hist(data_arr, 25, edgecolor = 'black', color='blue', alpha=0.5)
+ax1b = ax1.twinx()
+ax1b.hist(data_arr, 25, edgecolor = 'black', alpha=0.5)
+
 ax1.set_xlabel('Data Value')
 ax1.set_ylim([0, 1.5])
 ax1.set_ylabel('Density')
-ax2.set_ylabel('Frequency')
-plt.title('Histogram of Raw Data and Final Mixture Components')
+ax1b.set_ylabel('Frequency')
+
+x_ll = np.arange(0, len(log_likelihoods))
+ax2.plot(x_ll, log_likelihoods)
+ax2.set_xlabel('Iteration')
+ax2.set_ylabel('Log Likelihood + Log Prior')
+ax2.yaxis.set_label_position("right")
+ax2.yaxis.tick_right()
+ax1.set_title('Histogram of Raw Data and Final Mixture Components')
+ax2.set_title('Log Likelihood + Log Prior Over All Iterations')
 plt.show()
